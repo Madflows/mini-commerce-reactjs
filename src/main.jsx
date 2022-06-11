fdc149cf3058bd5c31bc5060b536677f389da1a1
Folarin Lawal <lawalfolarin6@gmail.com>
2022-06-11 23:48:37 +0000

    Realized i have to use HashRouter for gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { CartProvider } from 'react-use-cart';
 import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <CartProvider>
-      <Router>
+      <Router base="/">
         <Toaster />
         <App />
       </Router>
